1569159acfe58dcb9ffd3cbd3b4b507dbc9fb3e5
Kohei-uec <119389076+Kohei-uec@users.noreply.github.com>
2024-02-23 20:12:50 +0900

    Add files via upload

---
+++ crossValidation2.m
@@ -0,0 +1,27 @@
+function accuracy = crossValidation2(cv,idxp, idxn,data_pos, data_neg, learn, myClassify)
+    %cross validation
+    %cv = 5;
+    %idx = 1:100;
+    accuracy = zeros(cv);
+    
+    for i = 1:cv
+        train_pos=data_pos(find(mod(idxp,cv)~=(i-1)),:);
+        eval_pos =data_pos(find(mod(idxp,cv)==(i-1)),:);
+        train_neg=data_neg(find(mod(idxn,cv)~=(i-1)),:);
+        eval_neg =data_neg(find(mod(idxn,cv)==(i-1)),:);
+        
+        train=[train_pos; train_neg];
+        eval=[eval_pos; eval_neg];
+        
+        train_label=[ones(size(train_pos,1),1); ones(size(train_neg,1),1)*(-1)];
+        eval_label =[ones(size(eval_pos,1),1); ones(size(eval_neg,1),1)*(-1)];
+        %学習
+        model = learn(train, train_label);
+        %分類
+        [predicted_label, scores] = myClassify(model, eval);
+        %評価
+        ac = numel(find(eval_label==predicted_label))/numel(eval_label);
+        accuracy(i, :) = ac;
+    end
+
+end
